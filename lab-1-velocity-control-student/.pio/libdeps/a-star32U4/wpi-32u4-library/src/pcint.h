f9c908f9329e3ddffb2559854ec3c2e307736d6d
lcsanneman <lcsanneman@wpi.edu>
2023-10-30 13:43:16 -0400

    third times the charm

--- lab-1-velocity-control-student/.pio/libdeps/a-star32U4/wpi-32u4-library/src/pcint.h
+++
@@ -1,9 +0,0 @@
-#include <Arduino.h>
-
-/** \brief Attaches a function to a pin change interrupt.
- *
- * \param pcInt The specific pin change interrupt, e.g., "PCINT3"
- * \param pcisr A pointer to the ISR function, of type void fxn(void).
- *  */
-
-void attachPCInt(uint8_t pcInt, void (*pcisr)(void));
